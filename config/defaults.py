38d6e8f7255a25e01733d98e012f6b6abfc8bf88
Eric Youngberg <eyoungberg@mapc.org>
2019-04-09 08:42:14 -0400

    Change excel config to writer config

--- config/defaults.py
+++ config/defaults.py
@@ -7,9 +7,10 @@ DEFAULTS = {
     'ARGS_HEADLESS': False,
     'ARGS_INCLUDE_METADATA': False,
 
-    'EXCEL_MAX_COL_WIDTH': 40,
-
     'PRQL_HOST': 'https://prql.mapc.org',
 
     'WEB_PORT': 8001,
+
+    'WRITER_MAX_COL_WIDTH': 40,
+    'WRITER_MAX_COMPOSITIONS': 30,
 }
