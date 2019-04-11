9d2ce1860112acc618d1ee4deff6001edba6947c
Eric Youngberg <eyoungberg@mapc.org>
2019-04-11 09:42:47 -0400

    Change default compositions directory to proper name

--- config/defaults.py
+++ config/defaults.py
@@ -6,7 +6,7 @@ DEFAULTS = {
     'ARGS_FORMAT': 'csv',
     'ARGS_HEADLESS': False,
     'ARGS_INCLUDE_METADATA': False,
-    'ARGS_OUTPATH': 'src/web/components',
+    'ARGS_OUTPATH': 'src/web/compositions',
 
     'PRQL_HOST': 'https://prql.mapc.org',
 
